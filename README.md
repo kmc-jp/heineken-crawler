76930f8e689c6424a703221f0f9885c2e05eb109
nonylene <cookie.speaker@gmail.com>
2017-04-12 03:01:52 +0900

    add clients for dev

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # heineken-crawler
 Hayai Kensaku Pukiwiki Crawler
 
+## Usage
+
+- crawl
+
+```shell
+$ python3 crawl.py
+```
+
+- clients for dev
+
+```shell
+# show help
+$ python3 dev-client.py -h
+```
+
 ## Requirements
 
 Python 3.x
---
+++ dev-client.py
@@ -0,0 +1,62 @@
+import argparse
+import config
+import json
+
+from els.client import ElsClient
+
+client = ElsClient(config.ELASTIC_SEARCH_ENDPOINT, config.INDEX)
+
+def add_index(args):
+    with args.index_file as f:
+        client.add_index(f.read())
+
+def delete_index(args):
+    client.delete_index(index=args.index)
+
+def search(args):
+    query = {
+            "query": {
+                "query_string": {
+                    "query": args.query
+                    }
+                }
+            }
+    if args.title:
+        query["_source"] = ["title"]
+
+    body = client.search(json.dumps(query)).read().decode("utf-8")
+    print(json.loads(body))
+
+
+parser = argparse.ArgumentParser(description='Elastic search client')
+subparsers = parser.add_subparsers()
+
+parser_add = subparsers.add_parser('add-index', help='add index (see add-index -h)')
+parser_add.add_argument('index_file', type=argparse.FileType(), help='index config json file')
+parser_add.set_defaults(func=add_index)
+
+parser_delete = subparsers.add_parser('delete-index', help='delete index (see delete-index -h)')
+parser_delete.add_argument(
+        '-i',
+        '--index', 
+        default=config.INDEX,
+        type=str,
+        help="index for delete (default: %(default)s)"
+        )
+parser_delete.set_defaults(func=delete_index)
+
+parser_search = subparsers.add_parser('search', help='search (see search -h)')
+parser_search.add_argument('query', type=str, help='search query')
+parser_search.add_argument(
+        '-t',
+        '--title', 
+        action="store_true",
+        help="return title only"
+        )
+parser_search.set_defaults(func=search)
+
+args = parser.parse_args()
+if hasattr(args, 'func'):
+    args.func(args)
+else:
+    parser.print_help()
