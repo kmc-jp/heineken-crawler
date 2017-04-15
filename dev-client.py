c6853cd2a22201b18d0950b0524152fdac382298
nonylene <cookie.speaker@gmail.com>
2017-04-15 19:58:16 +0900

    always match query

--- dev-client.py
+++ dev-client.py
@@ -17,16 +17,18 @@ def search(args):
     query = {
             "query": {
                 "query_string": {
-                    "query": args.query
+                    # boost title
+                    "fields": ["title^5", "body"],
+                    "query": args.query,
+                    "auto_generate_phrase_queries": True
                     }
                 }
             }
-    if args.title:
-        query["_source"] = ["title"]
+    if args.title: query["_source"] = ["title"]
+    if args.explain: query["explain"] = True
 
     body = client.search(json.dumps(query)).read().decode("utf-8")
-    print(json.loads(body))
-
+    print(body)
 
 parser = argparse.ArgumentParser(description='Elastic search client')
 subparsers = parser.add_subparsers()
@@ -49,9 +51,15 @@ parser_search = subparsers.add_parser('search', help='search (see search -h)')
 parser_search.add_argument('query', type=str, help='search query')
 parser_search.add_argument(
         '-t',
-        '--title', 
+        '--title',
         action="store_true",
         help="return title only"
+        )
+parser_search.add_argument(
+        '-e',
+        '--explain',
+        action="store_true",
+        help="include explain"
         )
 parser_search.set_defaults(func=search)
 
