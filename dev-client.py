9743612c9ef80a1aced8912b934bfd5ae78ca231
nonylene <nonylene@gmail.com>
2022-01-02 01:56:48 +0900

    Update dev-client.py for current directory structure

--- dev-client.py
+++ dev-client.py
@@ -1,17 +1,10 @@
 import argparse
-import config
 import json
 
+from config import pukiwiki
 from els.client import ElsClient
 
-client = ElsClient(config.ELASTIC_SEARCH_ENDPOINT, config.INDEX)
-
-def add_index(args):
-    with args.index_file as f:
-        client.add_index(f.read())
-
-def delete_index(args):
-    client.delete_index(index=args.index)
+client = ElsClient(pukiwiki.ELASTIC_SEARCH_ENDPOINT, pukiwiki.INDEX)
 
 def search(args):
     query = {
@@ -20,31 +13,35 @@ def search(args):
                     # boost title
                     "fields": ["title^5", "body"],
                     "query": args.query,
+                    # "analyzer": "jp_search_analyzer",
+                    "default_operator": "AND",
+                    # "type": "phrase",
                     }
                 }
             }
     if args.title: query["_source"] = ["title"]
-    if args.explain: query["explain"] = True
+    if args.explain:
+        query["explain"] = True
+        query["profile"] = True
+    if args.highlight:
+        query["highlight"] = {
+            "encoder": "html",
+            "fields": {
+                "body": {
+                    "pre_tags": ["<mark>"],
+                    "post_tags": ["</mark>"],
+                    "fragment_size": 220,
+                    "no_match_size": 220,
+                    "number_of_fragments": 1,
+                }
+            }
+        }
 
     body = client.search(json.dumps(query)).read().decode("utf-8")
     print(body)
 
 parser = argparse.ArgumentParser(description='Elastic search client')
 subparsers = parser.add_subparsers()
-
-parser_add = subparsers.add_parser('add-index', help='add index (see add-index -h)')
-parser_add.add_argument('index_file', type=argparse.FileType(), help='index config json file')
-parser_add.set_defaults(func=add_index)
-
-parser_delete = subparsers.add_parser('delete-index', help='delete index (see delete-index -h)')
-parser_delete.add_argument(
-        '-i',
-        '--index', 
-        default=config.INDEX,
-        type=str,
-        help="index for delete (default: %(default)s)"
-        )
-parser_delete.set_defaults(func=delete_index)
 
 parser_search = subparsers.add_parser('search', help='search (see search -h)')
 parser_search.add_argument('query', type=str, help='search query')
@@ -59,6 +56,11 @@ parser_search.add_argument(
         '--explain',
         action="store_true",
         help="include explain"
+        )
+parser_search.add_argument(
+        '--highlight',
+        action="store_true",
+        help="include highlight"
         )
 parser_search.set_defaults(func=search)
 
