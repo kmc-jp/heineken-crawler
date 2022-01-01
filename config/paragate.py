8a6ec466eabbc8240fb8ac8c9bf2fc70d9e51d28
hiromi-mi <31833325+hiromi-mi@users.noreply.github.com>
2022-01-01 23:51:56 +0900

    update config to k8s

--- config/paragate.py
+++ config/paragate.py
@@ -1,5 +1,5 @@
 # dir includes wiki raw data
 PARAGATE_MAIL_DIR = "/home/kmc/bin/paragate/mail/"
-ELASTIC_SEARCH_ENDPOINT = "http://himekun:9200/"
+ELASTIC_SEARCH_ENDPOINT = "http://heineken-elasticsearch.sandbox.svc.cluster.local:9200/"
 INDEX = "mail"
 INDEX_FILE = "index/mail.json"
